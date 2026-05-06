b8b156c4951a75625d9b0f920fbcb561b3248a6b
vtu26421 <vtu26421@veltech.edu.in>
2026-05-06 13:55:04 +0530

    Delete TicketBookingBackend directory

--- TicketBookingBackend/booking-service/src/main/java/com/veltech/booking/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.veltech.booking.repository;
-
-import com.veltech.booking.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
